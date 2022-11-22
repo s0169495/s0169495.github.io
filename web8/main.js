7cd8299dc16943453f58dd1875929405141b4d48
s0169495 <113280998+s0169495@users.noreply.github.com>
2022-11-22 21:21:08 +0300

    Update main.js

--- web8/main.js
+++ web8/main.js
@@ -1,1 +1,15 @@
-onclick="history.back()"
+
+window.addEventListener("popstate", function(e) {
+    getContent(location.pathname, false);
+});
+
+
+document.addEventListener("DOMContentLoaded", function() { 
+    document.querySelectorAll('textarea, input').forEach(function(e) {
+        if(e.value === '') e.value = window.sessionStorage.getItem(e.name, e.value);
+        e.addEventListener('input', function() {
+            window.sessionStorage.setItem(e.name, e.value);
+        })
+    })
+
+}); 
