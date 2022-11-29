4d25707041f561dfa87d59b1a554de81dfae5dcd
s0169495 <113280998+s0169495@users.noreply.github.com>
2022-11-29 21:12:16 +0300

    Update main.js

--- web8/main.js
+++ web8/main.js
@@ -3,7 +3,7 @@ window.addEventListener("popstate", function(e) {
     getContent(location.pathname, false);
 });
 
-
+/*
 document.addEventListener("DOMContentLoaded", function() { 
     document.querySelectorAll('textarea, input').forEach(function(e) {
         if(e.value === '') e.value = window.sessionStorage.getItem(e.name, e.value);
@@ -13,3 +13,37 @@ document.addEventListener("DOMContentLoaded", function() {
     })
 
 }); 
+*/
+
+
+function persistInput(input)
+{
+  var key = "input-" + input.id;
+
+  var storedValue = localStorage.getItem(key);
+
+  if (storedValue)
+      input.value = storedValue;
+
+  input.addEventListener('input', function ()
+  {
+      localStorage.setItem(key, input.value);
+  });
+}
+
+
+
+
+
+window.onload = function(){
+
+    var a = document.getElementById("name");
+    persistInput(a);
+    var b = document.getElementById("post");
+    persistInput(b);
+    var c = document.getElementById("text");
+    persistInput(c);
+    var d = document.getElementById("ch");
+    persistInput(d);
+
+}
