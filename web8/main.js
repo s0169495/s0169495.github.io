ed73c7499926dc2c1acea7d7e73898b27e567d21
s0169495 <113280998+s0169495@users.noreply.github.com>
2022-11-29 22:18:55 +0300

    Update main.js

--- web8/main.js
+++ web8/main.js
@@ -1,20 +1,4 @@
 
-window.addEventListener("popstate", function(e) {
-    getContent(location.pathname, false);
-});
-
-/*
-document.addEventListener("DOMContentLoaded", function() { 
-    document.querySelectorAll('textarea, input').forEach(function(e) {
-        if(e.value === '') e.value = window.sessionStorage.getItem(e.name, e.value);
-        e.addEventListener('input', function() {
-            window.sessionStorage.setItem(e.name, e.value);
-        })
-    })
-
-}); 
-*/
-
 
 function goBack() {
     window.history.back()
