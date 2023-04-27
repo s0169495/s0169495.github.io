209a4ad4f5d6cdf2974b4980cfea87611b2584a1
s0169495 <113280998+s0169495@users.noreply.github.com>
2023-04-27 21:27:54 +0300

    Add files via upload

---
+++ d4/form.php
@@ -0,0 +1,120 @@
+
+<!DOCTYPE html>
+
+<html lang="ru">
+<head>
+<title>Задание</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="index.css">
+<style>
+.error {
+  border: 2px solid red;
+}
+    </style>
+</head>
+
+
+<body>
+    
+<?php
+if(!empty($messages)){
+    print('<div id="messages">');
+  foreach ($messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
+?>
+    <form action="index.php" method="POST">
+
+
+
+        <b><a>Форма</a></b></br></br>
+        <label <?php if ( $errors['userName'] || $errors['userName_wrong']) {print 'class="error"' ; } ?>>
+            Имя:<br />
+            <input name="field-name"
+                   placeholder="Введите ваше имя" value="<?php print $values['userName']; ?>"/>
+        </label><br />
+
+        <label <?php if ($errors['userEmail'] || $errors['userEmail_wrong']) {print 'class="error"';} ?>>
+            email:<br />
+            <input name="field-email"
+                   type="email"
+                   placeholder="Введите вашу почту" value="<?php print $values['userEmail']; ?>"/>
+        </label><br />
+
+        <br>
+        <label>
+            Год рождения:<br>
+
+
+            <select name="year" value="<?php print $values['userDate']; ?>">
+                <option value="1990" <?php if($values['userDate'] == 1990) {print 'selected';}?>>1990 год</option>
+                <option value="1991" <?php if($values['userDate'] == 1991) {print 'selected';}?>>1991 год</option>
+                <option value="1992" <?php if($values['userDate'] == 1992) {print 'selected';}?>>1992 год</option>
+                <option value="1993" <?php if($values['userDate'] == 1993) {print 'selected';}?>>1993 год</option>
+                <option value="1994" <?php if($values['userDate'] == 1994) {print 'selected';}?>>1994 год</option>
+                <option value="1995" <?php if($values['userDate'] == 1995) {print 'selected';}?>>1995 год</option>
+                <option value="1996" <?php if($values['userDate'] == 1996) {print 'selected';}?>>1996 год</option>
+                <option value="1997" <?php if($values['userDate'] == 1997) {print 'selected';}?>>1997 год</option>
+                <option value="1998" <?php if($values['userDate'] == 1998) {print 'selected';}?>>1998 год</option>
+              </select>
+              <br />     
+
+        Пол:<br />
+        
+            <input type="radio" checked="checked" <?php if($values['userGender'] == 0) {print 'checked="checked"';} ?>
+                   name="radio-group-1" value="0" />
+                   <label>М
+        </label>
+       
+            <input type="radio" <?php if($values['userGender'] == 1) {print 'checked="checked"';} ?>
+                   name="radio-group-1" value="1" />
+                   <label>Ж
+        </label><br />
+
+        Количество конечностей:<br />
+      
+            <input type="radio" checked="checked" <?php if($values['userLimbs'] == 4) {print 'checked="checked"';} ?>
+                   name="radio-group-2" value="4"/>
+                   <label>4
+        </label>
+        
+            <input type="radio" <?php if($values['userLimbs'] == 3) {print 'checked="checked"';} ?>
+                   name="radio-group-2" value="3" />
+                   <label>3
+        </label>
+       
+            <input type="radio" <?php if($values['userLimbs'] == 2) {print 'checked="checked"';} ?>
+                   name="radio-group-2" value="2" />
+                   <label>2
+        </label>
+        
+            <input type="radio" <?php if($values['userLimbs'] == 1) {print 'checked="checked"';} ?>
+                   name="radio-group-2" value="1" />
+                   <label>1
+        </label><br />
+
+        Сверхспособности:<label>
+            <br />
+            <select multiple name="field-name-3[]">
+            <option value=0 <?php if($values['Ab0'] == 1){print 'selected';}?>>Бессмертие</option>
+                <option value=1 <?php if($values['Ab1'] == 1){print 'selected';}?>>Прохождение сквозь стены</option>
+                <option value=2 <?php if($values['Ab2'] == 1){print 'selected';}?>>Левитация</option>
+            </select>
+        </label><br />
+
+        <label>
+            Биография:<br />
+            <textarea name="field-name-2"><?php print $values['userBio']; ?></textarea>
+        </label><br />
+
+
+        С контрактом ознакомлен (а):<br />
+
+
+        <input type="checkbox" id="ch" name="send-enabled" />
+        <input type="submit" name="send" value="Отправить" />
+    </form>
+</body>
+</html>
---
+++ d4/index.php
@@ -0,0 +1,145 @@
+<?php
+
+header('Content-Type: text/html; charset=UTF-8');
+
+function goBack(){
+    header('Location: index.php');
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+    $messages = array();
+    if (!empty($_COOKIE['save'])) {
+        setcookie('save', '', 100000);
+        $messages[] = '<div style="color:#191970;">Спасибо, результаты сохранены. </div>';
+    }
+
+    $errors = array();
+    $errors['userName'] = !empty($_COOKIE['userName_error']);
+    $errors['userEmail'] = !empty($_COOKIE['userEmail_error']);
+    $errors['userName_wrong'] = !empty($_COOKIE['userName_wrong']);
+    $errors['userEmail_wrong'] = !empty($_COOKIE['userEmail_wrong']);
+
+    if($errors['userName']) {
+        setcookie('userName_error', '', 100000);
+        $messages[] = '<div style="color:#8b0000;">Заполните имя.</div>';
+    }
+    if($errors['userEmail']) {
+        setcookie('userEmail_error', '', 100000);
+        $messages[] = '<div style="color:#8b0000;">Заполните email.</div>';
+    }
+ 
+    if($errors['userName_wrong']) {
+        setcookie('userName_wrong', '', 100000);
+        $messages[] = '<div style="color:#8b0000;">Используются недопустимые символы в поле "Имя".</div>';
+    }
+    if($errors['userEmail_wrong']) {
+        setcookie('userEmail_wrong', '', 100000);
+        $messages[] = '<div style="color:#8b0000;">Используются недопустимые символы в поле "Email".</div>';
+    }
+
+    $values = array();
+    $values['userName'] = empty($_COOKIE['userName_value']) ? '' : $_COOKIE['userName_value'];
+    $values['userEmail'] = empty($_COOKIE['userEmail_value']) ? '' : $_COOKIE['userEmail_value'];
+    $values['userDate'] = empty($_COOKIE['userDate_value']) ? '' : $_COOKIE['userDate_value'];
+    $values['userGender'] = empty($_COOKIE['userGender_value']) ? '' : $_COOKIE['userGender_value'];
+    $values['userLimbs'] = empty($_COOKIE['userLimbs_value']) ? '' : $_COOKIE['userLimbs_value'];
+    $values['userBio'] = empty($_COOKIE['userBio_value']) ? '' : $_COOKIE['userBio_value'];
+    $values['Ab0'] = empty($_COOKIE['userAb0']) ? '' : $_COOKIE['userAb0'];
+    $values['Ab1'] = empty($_COOKIE['userAb1']) ? '' : $_COOKIE['userAb1'];
+    $values['Ab2'] = empty($_COOKIE['userAb2']) ? '' : $_COOKIE['userAb2'];
+
+    include('form.php');
+
+}else{
+
+    $errors = FALSE;
+
+    if (empty($_POST['field-name'])) {
+        setcookie('userName_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else if(!preg_match('/^[a-zA-Zа-яА-Я]+$/ui', $_POST['field-name'])){
+        setcookie('userName_wrong', '3', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        $userName = $_POST['field-name'];
+        setcookie('userName_value', $userName, time() + 365 * 24 * 60 * 60);
+    }
+
+    if (empty($_POST['field-email'])) {
+        setcookie('userEmail_error', '2', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else if(!preg_match('/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/', $_POST['field-email'])){
+        setcookie('userEmail_wrong', '4', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        $userEmail = $_POST['field-email'];
+        setcookie('userEmail_value', $userEmail, time() + 365 * 24 * 60 * 60);
+    }
+
+        $userDate = $_POST['year'];
+        setcookie('userDate_value', $userDate, time() + 365 * 24 * 60 * 60);
+
+        $userGender = $_POST['radio-group-1'];
+        setcookie('userGender_value', $userGender, time() + 365 * 24 * 60 * 60);
+   
+        $userLimbs = $_POST['radio-group-2'];
+        setcookie('userLimbs_value', $userLimbs, time() + 365 * 24 * 60 * 60);
+
+        $userBio = $_POST['field-name-2'];
+        setcookie('userBio_value', $userBio, time() + 365 * 24 * 60 * 60);
+
+        $userAb = array();
+        $n = count($_POST['field-name-3']);
+        for ($i = 0; $i < $n; $i++) {
+            $userAb[$i] = $_POST['field-name-3'][$i];
+    }
+        for ($i = 0; $i < $n; $i++) {
+            if ($userAb[$i] == 0) {$Ab0=1;  setcookie('userAb0', 1, time() + 365 * 24 * 60 * 60);}
+            else if ($userAb[$i] == 1)  {$Ab1=1;  setcookie('userAb1', 1, time() + 365 * 24 * 60 * 60);}
+            else {$Ab2=1;  setcookie('userAb2', 1, time() + 365 * 24 * 60 * 60);}
+    }
+
+
+    if($errors){
+        header('Location: index.php');
+        exit();
+    }else {
+        setcookie('userName_error', '', 100000);
+        setcookie('userEmail_error', '', 100000);
+        setcookie('userName_wrong', '', 100000);
+        setcookie('userEmail_wrong', '', 100000);
+    }
+
+
+    try {
+        $user = 'u53720';
+        $pass = '8531034';
+        $db = new PDO('mysql:host=localhost;dbname=u53720', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+        $stmt_1 = $db->prepare("INSERT INTO app (name, email, yearper, gender, kol, bio) VALUES (:name, :email, :yearper, :gender, :kol, :bio)");
+        $stmt_1->bindParam(':name', $userName);
+        $stmt_1->bindParam(':email', $userEmail);
+        $stmt_1->bindParam(':yearper', $userDate);
+        $stmt_1->bindParam(':gender', $userGender);
+        $stmt_1->bindParam(':kol', $userLimbs);
+        $stmt_1->bindParam(':bio', $userBio);
+        $stmt_1->execute();
+        $id = $db->lastInsertId();
+
+        for ($i = 0; $i<$n; $i++){
+
+    
+        $stmt_2 = $db->prepare("INSERT INTO capapp (id, id_cap) VALUES (:id, :id_cap)");
+        $stmt_2->bindParam(':id', $id);
+        $stmt_2->bindParam(':id_cap', $userAb[$i]);
+        $stmt_2->execute();
+
+        }
+    } catch (PDOException $e) {
+        print('Error : ' . $e->getMessage());
+        exit();
+    }
+    setcookie('save', '1');
+    header('Location: ?save=1');
+}
